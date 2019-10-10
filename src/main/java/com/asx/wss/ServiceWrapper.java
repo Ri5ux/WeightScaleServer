b0013c2292657998e4db7811ec4eac6fa4cece4b
Ri5ux <36822012+Ri5ux@users.noreply.github.com>
2019-10-10 09:21:33 -0500

    Added console redirection.

--- src/main/java/com/asx/wss/ServiceWrapper.java
+++ src/main/java/com/asx/wss/ServiceWrapper.java
@@ -1,6 +1,8 @@
 package com.asx.wss;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 import com.asx.wss.Util.ComPortEntry;
@@ -10,15 +12,21 @@ import com.asx.wss.web.WebServer;
 
 public class ServiceWrapper
 {
-    private static final File        CONFIG_FILE   = new File("settings.json");
+    private static final File            CONFIG_FILE   = new File("settings.json");
 
-    private static SerialWeightScale scale;
-    private static boolean           appRunning    = false;
-    private static long              reconnectTime = 0;
-    private static Config            config;
+    private static SerialWeightScale     scale;
+    private static boolean               appRunning    = false;
+    private static long                  reconnectTime = 0;
+    private static Config                config;
+    private static String                error;
+
+    private static PrintStream           originalOutputBuffer;
+    private static PrintStream           redirectOutputBuffer;
+    private static ByteArrayOutputStream redirectOutputStream;
 
     public static void main(String[] args)
     {
+        redirectConsole();
         System.out.println("Service starting...");
         config = new Config(CONFIG_FILE);
         config.load();
@@ -70,22 +78,43 @@ public class ServiceWrapper
             catch (InterruptedException e)
             {
                 e.printStackTrace();
+                ServiceWrapper.setError("sleep_thread_error");
             }
         }
+    }
+
+    public static void redirectConsole()
+    {
+        redirectOutputStream = new ByteArrayOutputStream();
+        redirectOutputBuffer = new PrintStream(redirectOutputStream);
+        originalOutputBuffer = System.out;
+        
+        System.setOut(redirectOutputBuffer);
+    }
+
+    public static String getConsoleOutput()
+    {
+        return redirectOutputStream.toString();
+    }
+
+    public static void disableConsoleRedirect()
+    {
+        System.out.flush();
+        System.setOut(originalOutputBuffer);
     }
 
     public static SerialWeightScale getWeightScale()
     {
         return scale;
     }
-    
+
     public static void killScale()
     {
         if (scale != null && scale.isConnected())
         {
             scale.close();
         }
-        
+
         scale = null;
     }
 
@@ -103,9 +132,19 @@ public class ServiceWrapper
     {
         scale = null;
     }
-    
+
     public static Config config()
     {
         return config;
+    }
+
+    public static String getError()
+    {
+        return error;
+    }
+
+    public static void setError(String error)
+    {
+        ServiceWrapper.error = error;
     }
 }
